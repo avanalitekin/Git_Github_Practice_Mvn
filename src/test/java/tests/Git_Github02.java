259d50b6fe2561ee070fb16c42f9e9691d3a3d69
alitekin <avanalitekin@gmail.com>
2019-08-07 10:19:27 -0500

    src/test/java/tests/Git_Github02.java class added

---
+++ src/test/java/tests/Git_Github02.java
@@ -0,0 +1,23 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class Git_Github02 {
+    static WebDriver driver=Driver.getDriver();
+    @Test
+    public void test01(){
+        driver.get("https://www.google.com");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        WebElement search_box=driver.findElement(By.xpath("//*[@title='Search']"));
+        search_box.sendKeys(("github"+ Keys.ENTER));
+        Driver.getDriver().close();
+    }
+}
