bc93bab5b7af60e94cfaa187fc14d4fc34bb709d
alitekin <avanalitekin@gmail.com>
2019-08-07 10:41:00 -0500

    Calculator file added with add  method

---
+++ src/test/java/tests/Calculator.java
@@ -0,0 +1,11 @@
+package tests;
+
+public class Calculator {
+    public static void main(String[] args){
+        add(10,15);
+
+    }
+    public static int add(int n1, int n2){
+        return n1+n2;
+    }
+}
