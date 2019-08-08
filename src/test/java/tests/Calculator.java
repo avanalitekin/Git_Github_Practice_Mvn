19b256cf789f67f769162d30c59be108a15b937a
avanalitekin <46761048+avanalitekin@users.noreply.github.com>
2019-08-08 10:14:42 -0500

    main method has divide implemenation

--- src/test/java/tests/Calculator.java
+++ src/test/java/tests/Calculator.java
@@ -2,6 +2,7 @@ package tests;
 public class Calculator {
     public static void main(String[] args){
         add(10,15);
+        System.out.println(divide(15,3));
     }
     public static int add(int n1, int n2){
         return n1+n2;
