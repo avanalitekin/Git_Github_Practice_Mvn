52c7f4dfd311c329ea65a4b56210b4d7d3be88a0
alitekin <avanalitekin@gmail.com>
2019-08-08 10:15:16 -0500

    main method updated

--- src/test/java/tests/Calculator.java
+++ src/test/java/tests/Calculator.java
@@ -1,7 +1,8 @@
 package tests;
 public class Calculator {
     public static void main(String[] args){
-        add(10,15);
+        System.out.println(add(10,15));
+        System.out.println(subtract(15,10));
     }
     public static int add(int n1, int n2){
         return n1+n2;
