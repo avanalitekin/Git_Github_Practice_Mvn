ded1f0d6d592ca25ebc5939cf9ddc3448e18b490
avanalitekin <46761048+avanalitekin@users.noreply.github.com>
2019-08-08 09:45:32 -0500

    multiply method is added in calculator

--- src/test/java/tests/Calculator.java
+++ src/test/java/tests/Calculator.java
@@ -11,4 +11,8 @@ public class Calculator {
     public static int subtract(int number1, int number2){
         return number1-number2;
     }
+    
+    public static int multiply(int number1, int number2){
+        return number1*number2;
+    }
 }
