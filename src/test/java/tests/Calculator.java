e8117976135820f50dd148d87a37916a227b21cc
avanalitekin <46761048+avanalitekin@users.noreply.github.com>
2019-08-08 11:05:22 -0500

    divide method edit

--- src/test/java/tests/Calculator.java
+++ src/test/java/tests/Calculator.java
@@ -15,4 +15,7 @@ public class Calculator {
     public static int multiply(int n1, int n2){
         return n1*n2;
     }
+    public static int divide(int n1, int n2){
+        return n1/n2;
+    }
 }
