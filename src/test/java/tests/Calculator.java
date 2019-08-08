5ccd169a632e784fad17d056cc1f2361cff5e0ec
alitekin <avanalitekin@gmail.com>
2019-08-08 08:59:00 -0500

    subtract method updated

--- src/test/java/tests/Calculator.java
+++ src/test/java/tests/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
     public static int add(int n1, int n2){
         return n1+n2;
     }
-    public static int subtract(int n1, int n2){
-        return n1-n2;
+    public static int subtract(int number1, int number2){
+        return number1-number2;
     }
 }
